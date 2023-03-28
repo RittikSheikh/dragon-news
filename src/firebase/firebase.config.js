b7ad673d928778f74f793414542218828d77896a
rittik sheikh <rittik11500@gamil.com>
2023-03-28 06:01:16 +0600

    structure created

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCzNWg7waKDq16EQzihmV36IFln0XEQa_Q",
+  authDomain: "dragon-news-81fab.firebaseapp.com",
+  projectId: "dragon-news-81fab",
+  storageBucket: "dragon-news-81fab.appspot.com",
+  messagingSenderId: "1051736090884",
+  appId: "1:1051736090884:web:823504586bae2226724851"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
